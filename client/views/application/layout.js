fe3bdd9f4b2070133aad769c8cf8ba5c819ea03e
drenfr01 <duncanrenfrow@gmail.com>
2014-04-02 23:12:44 -0400

    Removing dead code from layout.js

--- client/views/application/layout.js
+++ client/views/application/layout.js
@@ -7,11 +7,3 @@ Template.layout.currentCustomerName = function() {
   return '';
 };
 
-Template.layout.isCustomerLoggedIn = function() {
-  customerLoggedIn = false;
-  if(!_.isUndefined(Session.get('currentCustomer'))) {
-    customerLoggedIn = true;
-  }
-  return customerLoggedIn;
-};
-
