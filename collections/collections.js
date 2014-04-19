f3dfc9767855cd38a71e5d7f32f02b999d1c42d2
drenfr01 <duncanrenfrow@gmail.com>
2014-04-19 19:41:16 -0400

    Removing stupid division I put into collections.js

--- collections/collections.js
+++ collections/collections.js
@@ -2,23 +2,23 @@ Customers = new Meteor.Collection('customers');
 Forms = new Meteor.Collection('forms');
 StyleChoices = new Meteor.Collection('styleChoices');
 Orders = new Meteor.Collection('orders');
-if(Meteor.isClient) {
-  Images = new FS.Collection("images", {
-    stores: [new FS.Store.FileSystem("images", {path: "/Users/duncanrenfrow-symon/Documents/Meteor_App/apparel/uploads", maxTries:10})]
-  });
-  FS.debug = true;
+Images = new FS.Collection("images", {
+  stores: [new FS.Store.FileSystem("images", {path: "/Users/duncanrenfrow-symon/Documents/Meteor_App/apparel/uploads", maxTries:10})]
+});
+FS.debug = true;
 
-  //TODO: Should this run only on server?
-  //TODO: obviously change trivially true return when we implement user login
-  Images.allow({
-    insert: function(userId, doc) {
-      return true;
-    },
-    update: function(userId, doc, fieldNames, modifier) {
-      return true;
-    },
-    remove: function(userId, doc) {
-      return true;
-    }
-  });
-}
+//TODO: obviously change trivially true return when we implement user login
+Images.allow({
+  insert: function(userId, doc) {
+    return true;
+  },
+  update: function(userId, doc, fieldNames, modifier) {
+    return true;
+  },
+  remove: function(userId, doc) {
+    return true;
+  },
+  download: function(userId, doc) {
+    return true;
+  }
+});
