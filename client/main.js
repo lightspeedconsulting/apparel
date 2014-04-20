e1b4716d9e3bfc5237eb0ab5bacef25abbb6c096
Tyler Sheffels <Tyler.Sheffels@gmail.com>
2014-04-19 20:37:50 -0400

    Meteor reloads will no longer refresh the database, add uploads to gitignore

--- client/main.js
+++ client/main.js
@@ -4,8 +4,3 @@ Meteor.subscribe('errors');
 Meteor.subscribe('styleChoices');
 Meteor.subscribe('orders');
 Meteor.subscribe('images');
-if ( Meteor.is_client ) {
-    Meteor.startup(function () {
-        Session.set('currentCustomer', undefined);
-    });
-}
