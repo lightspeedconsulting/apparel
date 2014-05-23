586e3fb0eee0f0a361ea5c9f08618545c16a9afb
Tyler Sheffels <Tyler.Sheffels@gmail.com>
2014-05-22 22:15:52 -0400

    Adding ability to add a bunch of pictures to an item

--- client/views/itemPictures.js
+++ client/views/itemPictures.js
@@ -1,31 +1,33 @@
 //TODO: Both of these functions are similar to those in takePicture
 //We should refactor them and the ones in takePicture to take in a
-//selector parameter and then place the single functions in a 
+//selector parameter and then place the single functions in a
 //js helper folder on the client
-function insertFiles(files, e, view) {
-    FS.Utility.eachFile(e, function(file) {
-      var newFile = new FS.File(file);
-      newFile.metadata = {customerId: Session.get('currentCustomer'),
-        view: view};
-      Images.insert(newFile, function(error, fileObj) {
-        if(error) {
-          throwError(error.reason);
-        }
-      });
+//I'm also worried that we are going to have overlap with the function names in
+// takePicture.js
+function insertItemFiles(e, view) {
+  FS.Utility.eachFile(e, function(file) {
+    var newFile = new FS.File(file);
+    newFile.metadata = {customerId: Session.get('currentCustomer'),
+      view: Session.get('itemId')};
+    Images.insert(newFile, function(error, fileObj) {
+      if(error) {
+        throwError(error.reason);
+      }
     });
+  });
 }
 
 function removeImages(view) {
-  //TODO: note that Minimongo doesn't yet support findAndModify, so 
+  //TODO: note that Minimongo doesn't yet support findAndModify, so
   //we have to do this clunky approach
-  var image = Images.findOne({"metadata.view": view, 
-    "metadata.customerId": Session.get('currentCustomer')}, 
+  var image = Images.findOne({"metadata.view": view,
+    "metadata.customerId": Session.get('currentCustomer')},
     {sort: {updatedAt: -1}, limit: 1});
-  
+
     Images.remove(image._id);
-    
-  //TODO: ideally we would wait to batch submit the images, but 
-  //I couldn't think of a good way to store the event until a user 
+
+  //TODO: ideally we would wait to batch submit the images, but
+  //I couldn't think of a good way to store the event until a user
   //pressed a submit button
   Meteor.call('removeImage', image.id, function(error, imageId) {
     if(error) {
@@ -35,34 +37,33 @@ function removeImages(view) {
 }
 Template.itemPictures.events({
   'change #itemPicture': function(e) {
-    e.preventDefault(); 
-    
-    var files = e.target.files; 
-    insertFiles(files, e, "Item");
-  },
-  'click #removeItem': function(e) {
-    e.preventDefault();
-  
-    removeImages("Front");    
-  },
-  'click #doneItemPictures': function(e) {
     e.preventDefault();
 
-    Router.go('itemMenu');
-    throwError("Photos successfully added to item", "alert-success");
+    insertItemFiles(e, Session.get('currentCustomer'));
   },
-  'click #addAnotherPicture': function(e) {
-    e.preventDefault();
+  // 'click #removeItem': function(e) {
+  //   e.preventDefault();
 
-    Router.go('itemPictures');
-    throwError("Photo Added", "alert-success");
-  },
+  //   removeImages("Front");
+  // },
+  // 'click #doneItemPictures': function(e) {
+  //   e.preventDefault();
+
+  //   Router.go('itemMenu');
+  //   throwError("Photos successfully added to item", "alert-success");
+  // },
+  // 'click #addAnotherPicture': function(e) {
+  //   e.preventDefault();
+
+  //   Router.go('itemPictures');
+  //   throwError("Photo Added", "alert-success");
+  // },
 });
 
-Template.takePicture.helpers({
-  images: function(view) {
-    return Images.find({"metadata.view": view, 
-      "metadata.customerId": Session.get('currentCustomer')}, 
-      {sort: {updatedAt: -1}, limit: 1});
+Template.itemPictures.helpers({
+  images: function() {
+    return Images.find({"metadata.view": Session.get('itemId'),
+      "metadata.customerId": Session.get('currentCustomer')},
+      {sort: {updatedAt: -1}});
   }
 });
